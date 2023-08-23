bc1dd6ee98a9d783ec0ba38648dbb193da619ab7
katarinaramer <katarina.ramer@iths.se>
2023-08-23 20:41:49 +0200

    Started Mastermind

--- LaborationRefactoring/MastermindGame.cs
+++ LaborationRefactoring/MastermindGame.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using System.Text;
+
 namespace LaborationRefactoring;
 
 internal class Mastermind : IGame
 {
-    private const string GameName = "Mastermind"; 
+    private const string GameName = "Mastermind";
 
     IUI io;
     IDAO dAO;
@@ -21,8 +23,63 @@ internal class Mastermind : IGame
 
     public void RunGame()
     {
-        Console.WriteLine("You are playing Mastermind. Lucky you!");
-        Thread.Sleep(1500);
+        Console.WriteLine("You are playing Mastermind");
     }
 }
+
+    //    bool playOn = true;
+    //    string playerName = io.GetUserName();
+
+
+    //    while (playOn)
+    //    {
+    //        string answer = GenerateNewAnswer();
+    //        string playerGuess;
+    //        int numberOfGuesses = 0;
+    //        string? answerFeedbackBullsOrCows = null;
+
+    //        io.StartNewGame(answer);
+
+    //        while (answerFeedbackBullsOrCows != "BBBB,")
+    //        {
+    //            numberOfGuesses++;
+    //            playerGuess = io.GetGuess();
+
+    //            answerFeedbackBullsOrCows = CompareGuessToAnswer(answer, playerGuess);
+    //            io.ShowGuessFeedback(answerFeedbackBullsOrCows);
+    //        }
+
+    //        dAO.AddMooResults(playerName, numberOfGuesses);
+
+    //        ShowTopList();
+
+    //        io.ShowRoundFeedback(numberOfGuesses);
+
+    //        playOn = io.ContinueOrQuit();
+    //    }
+    //}
+
+    //private string GenerateNewAnswer()
+    //{
+        
+    //        string allowedLetters = "rbgywb";
+    //        Random randomGenerator = new Random();
+    //        char[] code = new char[4];
+
+    //        for (int i = 0; i < code.Length; i++)
+    //        {
+    //            int randomIndex = randomGenerator.Next(allowedLetters.Length);
+    //            code[i] = allowedLetters[randomIndex];
+    //        }
+
+    //        return new string(code);  
+    //}
+    
+
+
+
+
+
+    
+
 
